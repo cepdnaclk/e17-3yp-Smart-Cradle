e20c2127eb742bf779b5a12b6d53ccf13a39bd19
WijesooriyaHD <e17407@eng.pdn.ac.lk>
2021-08-26 15:31:37 +0530

    updated signup.dart - add input fields

--- Front End/cradle_app/lib/signup.dart
+++ Front End/cradle_app/lib/signup.dart
@@ -48,10 +48,16 @@ class SignupPage extends StatelessWidget {
               ),
               Column(
                 children: <Widget>[
+                  
                   inputFile(label: "Username"),
-                  inputFile(label: "Email"),
+                  
                   inputFile(label: "Password", obscureText: true),
                   inputFile(label: "Confirm Password ", obscureText: true),
+
+                  inputFile(label: "Address"),
+                  inputFile(label: "Mobile Number"),
+                  inputFile(label: "Device ID"),
+
                 ],
               ),
               Container(
