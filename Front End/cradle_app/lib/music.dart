61b9c17538f2e228a4100977b541cb539b6209d9
Shashini Upekha <e17356@eng.pdn.ac.lk>
2021-08-27 16:45:40 +0530

    add music.dart

---
+++ Front End/cradle_app/lib/music.dart
@@ -0,0 +1,169 @@
+import 'package:flutter/material.dart';
+
+
+class MusicPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        elevation: 0,
+        brightness: Brightness.light,
+        backgroundColor: Colors.white,
+        leading: IconButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          icon: Icon(Icons.arrow_back_ios,
+          size: 20,
+          color: Colors.black,),
+
+
+        ),
+      ),
+      body: Container(
+        height: MediaQuery.of(context).size.height,
+        width: double.infinity,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            Expanded(child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                Column(
+                  children: <Widget>[
+                    Text("Music",
+                    style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold,color:Colors.indigo[700]),),
+                    SizedBox(height: 20,),
+                    
+                    Text("Play some music to soothe your baby...",
+                    style: TextStyle(
+                      fontSize: 20,
+                    color:Colors.indigo[700]),)
+                  ],
+                ),
+               Container(
+                height: MediaQuery.of(context).size.height / 2,
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage("assets/images/home.jpg") //add  an image to welcome page
+                  )
+                ),
+              ),
+              Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 100),
+                  child: Row(
+                    children:<Widget>[
+
+                /*
+                Container(
+                height: MediaQuery.of(context).size.height / 3,
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage("assets/images/home.jpg") //add  an image to welcome page
+                  )
+                ),
+              ),
+                     */ 
+                     
+                    ],
+                  ),
+                
+
+                
+                ),
+             
+                  
+              Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 100),
+                  child: Row(
+                    children:<Widget>[
+
+                       IconButton(
+                        iconSize: 100,
+                        color: Colors.green[900],
+                        icon: Icon(Icons.play_circle),
+                        onPressed: () => print('play music'),
+        
+                       ),
+        
+                       IconButton(
+                        iconSize: 100,
+                        color: Colors.red[900],
+                        icon: Icon(Icons.stop),
+                        onPressed: () => print('off music'),
+                      ),
+                      
+                     
+                    ],
+                  ),
+                
+
+                
+                ),
+
+               
+
+
+
+              ],
+            ))
+          ],
+        ),
+      ),
+    );
+  }
+
+}
+
+
+// we will be creating a widget for icons
+ /*Widget buildNormalIcons() => GridView(
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+        ),
+        children: [
+          IconButton(
+            iconSize: 80,
+            color: Colors.blue,
+            icon: Icon(Icons.videocam),
+            onPressed: () => print('You can moniter the baby now'),
+          ),
+         
+
+          IconButton(
+            iconSize: 80,
+            color: Colors.yellow,
+            icon: Icon(Icons.music_note),
+            onPressed: () => print('Play Music'),
+          ),
+
+          IconButton(
+            iconSize: 80,
+            color: Colors.green,
+            icon: Icon(Icons.crib),
+            onPressed: () => print('Swing the cardle'),
+        
+          ),
+        
+          IconButton(
+            iconSize: 80,
+            color: Colors.red,
+            icon: Icon(Icons.thermostat),
+            onPressed: () => print('Check the temperature'),
+          ),
+        
+          IconButton(
+            iconSize: 80,
+            color: Colors.blue,
+            icon: Icon(Icons.videocam),
+            onPressed: () => print('You can moniter the baby now'),
+          ),
+        
+        
+        
+        
+        
+        ],
+      );*/
