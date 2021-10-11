de4ea0142c3197ae2c7c9e7187d59aef6b1cda6e
WijesooriyaHD <e17407@eng.pdn.ac.lk>
2021-10-11 16:16:46 +0530

    updated login page

--- Front End/cradle_app/lib/screens/login.dart
+++ Front End/cradle_app/lib/screens/login.dart
@@ -1,267 +1,110 @@
 import 'package:flutter/material.dart';
-import 'package:form_field_validator/form_field_validator.dart';
+import 'InputDeco_design.dart';
+import 'package:test/screens/otp.dart';
 
-import 'package:cradle_app/otp.dart';
-
-class LoginPageNew extends StatefulWidget {
+class LoginPage extends StatefulWidget {
   @override
   _LoginPageState createState() => _LoginPageState();
 }
 
-class _LoginPageState extends State<LoginPageNew> {
+class _LoginPageState extends State<LoginPage> {
 
-  GlobalKey <FormState> formkey = GlobalKey <FormState>();
+  String name;
 
-  void validate(){
+  //TextController to read text entered in text field
+  TextEditingController password = TextEditingController();
+  TextEditingController confirmpassword = TextEditingController();
 
-    if(formkey.currentState!.validate()){
-      print("Not valid");
-    }else{
-      print("Valid");
-    }
-
-  }
-
-  String validatePassword(value){
-
-    if(value.isEmpty){
-      return "required *";
-    }else if (value.length < 6 ){ 
-      return "should be atleast 6 characters";
-    }else if (value.length >15){
-      return "should not be more than 15 characters";
-    }else{
-      return " ";
-    }
-
-
-  }
+  final GlobalKey<FormState> _formkey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
-      resizeToAvoidBottomInset: false,
-      backgroundColor: Colors.white,
-      appBar: AppBar(
-        elevation: 0,
-        brightness: Brightness.light,
-        backgroundColor: Colors.white,
-        leading: IconButton(
-          onPressed: () {
-            Navigator.pop(context);
-          },
-          icon: Icon(Icons.arrow_back_ios,
-          size: 20,
-          color: Colors.black,),
-
-
-        ),
-      ),
-
-      body: Container(
-        height: MediaQuery.of(context).size.height,
-        width: double.infinity,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: <Widget>  [
-            Expanded(child: Column(
-
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: <Widget>[
-                Column(
-                    children: <Widget>[
-                    Text("Login",
-                    style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),),
-                    SizedBox(height: 20,),
-                    Text("Login to your account",
-                    style: TextStyle(
-                      fontSize: 15,
-                    color:Colors.grey[700]),)
-                  ],
+      body: Center(
+        child: SingleChildScrollView(
+          child: Form(
+            key: _formkey,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                
+               CircleAvatar(
+                  backgroundColor: Colors.white,
+                  radius:70,
+                  child: Image.asset("assets/images/login2.jpg"),
                 ),
-            
-
+                SizedBox(
+                  height: 10,
+                ),
                 Padding(
-                  padding: EdgeInsets.all(
-                    15.0
-                    
+                  padding: const EdgeInsets.only(bottom:10,left: 10,right: 10),
+                  child: TextFormField(
+                    keyboardType: TextInputType.text,
+                    decoration: buildInputDecoration(Icons.person,"Username"),
+                    validator: (String value){
+                      if(value.isEmpty)
+                      {
+                        return 'Please Enter Name';
+                      }
+                      return null;
+                    },
+                    onSaved: (String value){
+                      name = value;
+                    },
                   ),
-
-                  child: Center(
-                    child:Form (
-                      // ignore: deprecated_member_use
-                      autovalidate: true,
-                      key: formkey,
-                      child: Column(
-                        children: <Widget>[
-                          
-                          TextFormField(
-                            decoration: InputDecoration(
-                              //fillColor: Colors.purple,
-                              
-                              border: OutlineInputBorder(),labelText: "Username"),
-                              
-                              validator: MultiValidator(
-                                [
-                                  RequiredValidator(errorText: "required *"),
-
-                                ]
-                              ),
-
-                            
-                          ),
+                ),
+                
+                
                 Padding(
-                  padding: EdgeInsets.only(
-                    top:15.0
+                  padding: const EdgeInsets.only(bottom: 10,left: 10,right: 10),
+                  child: TextFormField(
+                    controller: password,
+                    obscureText: true,
+                    keyboardType: TextInputType.text,
+                    decoration:buildInputDecoration(Icons.lock,"Password"),
+                    validator: (String value){
+                      if(value.isEmpty)
+                      {
+                        return 'Please a Enter Password';
+                      }
+                      return null;
+                    },
+
                   ),
+                ),
+                
 
-                  child: Center(
-                    child:Form (
-                  
-                      child: Column(
-                        children: <Widget>[
+                SizedBox(
+                  width: 200,
+                  height: 50,
+                  child: RaisedButton(
+                    color: Colors.purple[700],
+                    onPressed: (){
 
-                          TextFormField(
-                            obscureText: true,
-                            decoration: InputDecoration(
+                      if(_formkey.currentState.validate())
+                      {
+                        print("successful");
+                        Navigator.push(context, MaterialPageRoute(builder: (context)=> Otp()));
 
-                              border: OutlineInputBorder(),labelText: "Password"),
-
-                              validator: validatePassword
-                          ),
-
+                       // return;
+                      }else{
+                        print("UnSuccessfull");
+                      }
+                    },
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(50.0),
+                        side: BorderSide(color: Colors.black,width:2)
                         
-                Padding(padding:
-                  EdgeInsets.only(top: 30),
-                  child: Container(
-                      padding: EdgeInsets.only(top: 0, left: 0), //3
-                      decoration:
-                        BoxDecoration(
-                          borderRadius: BorderRadius.circular(50),
-                          border: Border(
-                            bottom: BorderSide(color: Colors.black),
-                            top: BorderSide(color: Colors.black),
-                            left: BorderSide(color: Colors.black),
-                            right: BorderSide(color: Colors.black),
-
-                          )
-
-
-
-                        ),
-                      child: MaterialButton(
-                        minWidth: double.infinity,
-                        height: 60,
-                        onPressed: () {
-
-                          Navigator.push(context, MaterialPageRoute(builder: (context)=> Otp()));
-                        },
-                        color: Colors.purple[700],
-                        elevation: 0,
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(50),
-
-                        ),
-                        child: Text(
-                          "Login", style: TextStyle(
-                          fontWeight: FontWeight.w600,
-                          fontSize: 18,
-                          color: Colors.white,
-
-                        ),
-                        ),
-
-                      ),
                     ),
+                    textColor:Colors.white,child: Text("Login"),
+
                   ),
-
-
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    Text("Don't have an account?"),
-                    Text(" Sign up", style: TextStyle(
-                      fontWeight: FontWeight.w600,
-                      fontSize: 18,
-
-                    ),)
-                  ],
-                ),
-
-                  
-                Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 110), //250
-                  child: Row(
-                    children:<Widget>[
-
-                       IconButton(
-                        iconSize: 80,
-                        
-                        color: Colors.blue,
-                        icon: Icon(Icons.lock),
-                        onPressed: validate,
-        
-                       ),
-        
-                      
-                     
-                    ],
-                  ),
-                
-
-                
-                ),
-                          
-                        ],
-                        ) ,
-                      
-                      )
-                     ),
-            
-
-                ),
-
-                          
-                        ],
-                        ) ,
-                      
-                      )
-                     ),
-            
-
-                ),
-            
-            
+                )
               ],
-
-            )
-            )
-
-          ],
-
-
-
-
-
-
-
-
-
-
+            ),
+          ),
         ),
-
-
-
-
-
       ),
-
-
     );
   }
-    
 }
-
-  
-  
