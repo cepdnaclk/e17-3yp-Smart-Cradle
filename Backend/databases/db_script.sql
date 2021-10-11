ee21f404a9a8f93c3358675e2a33300f7c477ddd
WijesooriyaHD <e17407@eng.pdn.ac.lk>
2021-10-11 23:27:59 +0530

    add new db script

--- Backend/databases/db_script.sql
+++ Backend/databases/db_script.sql
@@ -5,19 +5,35 @@ create table DEVICE (
 
 device_id INT primary key,
 temperature float,
-cry_analysis bool,
-fan bool
+cry_analysis bool default false,
+fan bool default false
 );
 
 create table USER (
 
 user_name varchar(10) NOT NULL,
 password varchar(15) NOT NULL,
-address varchar(50) NOT NULL,
+email varchar(50) NOT NULL,
 mobile_number varchar(10) NOT NULL,
 device_id INT NOT NULL,
 
-primary key(user_name,password),
+primary key(user_name),
 foreign key (device_id) references DEVICE(device_id)
 
 );
+
+create table OWNERSHIP (
+
+device_id INT NOT NULL,
+user_name varchar(10) NOT NULL,
+
+primary key(device_id),
+foreign key (user_name) references USER(user_name)
+
+);
+
+
+show databases
+
+
+
