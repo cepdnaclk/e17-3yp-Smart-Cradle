ab6e80837c8d530f9280d450fa118bc2139c9f3c
WijesooriyaHD <e17407@eng.pdn.ac.lk>
2021-10-16 13:22:01 +0530

    signup.js validation done

--- Backend/server/src/routes/signup.js
+++ Backend/server/src/routes/signup.js
@@ -1,18 +1,44 @@
 const express = require('express')
 //const bcrypt = require('bcrypt')
+
+const Joi =require('joi')
 const Router = express.Router()
 var mysqlConnection = require('../connection/connection')
 
 Router.post('/', function(req, res) {
   
 
-    const {user_name,email,mobile_number,device_id,password}=req.body;
+    const {user_name,email,mobile_number,device_id,password,conf_password}=req.body;
     console.log(req.body);
+
+    //server side validation
+
+    const schema = Joi.object({
+
+        user_name: Joi.string().max(10).required(),
+        email: Joi.string().email().max(50).required(),
+        mobile_number:Joi.string().pattern(/^[0-9]+$/).min(10).max(10).required(),
+        device_id:Joi.string().pattern(/^[0-9]+$/).required(),
+        password:Joi.string().min(6).max(15).required(),
+        conf_password:Joi.any().valid(Joi.ref('password')).required()
+    
+    });
+
+    const result = schema.validate(req.body);
+
+    if(result.error){
+
+        res.status(400).send(result.error.details[0].message);
+        return;
+    }
+
+
 
     mysqlConnection.query('insert into DEVICE (device_id) values(?);',
     [device_id],(error,rows,fileds)=>{
+       
 
-       mysqlConnection.query('insert into USER (user_name,password,email,mobile_number,device_id) values(?,?,?,?,?);',
+        mysqlConnection.query('insert into USER (user_name,password,email,mobile_number,device_id) values(?,?,?,?,?);',
         [user_name,password,email,mobile_number,device_id ],(error,rows,fileds)=>{
 
          mysqlConnection.query('insert into OWNERSHIP (device_id,user_name) values(?,?);',
