2b9ac572ace0c4e5327374aeb0197b2eb4fdef72
WijesooriyaHD <e17407@eng.pdn.ac.lk>
2021-10-17 21:14:44 +0530

    update select.js

--- Backend/server/src/routes/select.js
+++ Backend/server/src/routes/select.js
@@ -3,22 +3,34 @@ const{Router} = require('express');
 const router = Router();
 
 const mysqlConnection =require('../connection/connection');
+const jwt =require('jsonwebtoken')
+const  ensureToken  = require('../middleware/authenticate')
 
 var sql = "SELECT device_id  from OWNERSHIP WHERE user_name = ?";
-router.get('/',(req,res)=>{
+router.get('/',ensureToken,(req,res)=>{
 
-    const {user_name,device_id}=req.body;
+    const {user_name}=req.body;
     console.log(req.body);
 
+    jwt.verify(req.token,'my_secret_key',function(error,data){
 
-    mysqlConnection.query(sql,[user_name],(error,rows,fileds)=>{
-
-        if(!error){
-            res.json(rows);
+        if(error){
+            res.sendStatus(403);
         }else{
-            console.log(error);
+            mysqlConnection.query(sql,[user_name],(error,rows,fileds)=>{
+
+                if(!error){
+                    res.json(rows);
+                }else{
+                    console.log(error);
+                }
+            });
+
         }
-    });
+    })
+
+
+  
 });
 
 
